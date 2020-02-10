2b6853193ce9a558c575063bb7322c4cb961fe10
Paul <admin@pauls-macbook-pro.home>
2020-02-10 23:45:34 +0100

    Initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import './global.scss';
+
+export default function Index() {
+  return (
+    <div>
+      <header>
+        <img src='/logo.svg' alt="logo"/>
+      </header>
+      <section>
+        <h1>Web development, made with love.</h1>
+        <h2>Front end / Javascript / Motion design integration</h2>
+        <p>Website opening soon !</p>
+      </section>
+    </div>
+  );
+}
